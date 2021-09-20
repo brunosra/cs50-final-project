d75b901085c21686814763bf5ff436f92a499c9c
Bruno Almeida <bruno@mcontigo.com>
2021-09-20 08:27:05 +0100

    renamed tables

--- cocciep/models/subjects.py
+++ cocciep/models/subjects.py
@@ -1,5 +1,6 @@
 from cocciep.db import db
 
 class Subject(db.Model):
+  __tablename__ = 'subjects'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(128))
--- cocciep/models/user.py
+++ cocciep/models/user.py
@@ -1,6 +1,7 @@
 from cocciep.db import db
 
 class User(db.Model):
+  __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(128))
   password_hash = db.Column(db.String(218))
