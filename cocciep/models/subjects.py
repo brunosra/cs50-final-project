4d06ecbd739af776462ba43eb6001f4443b18592
Bruno Almeida <bruno@mcontigo.com>
2021-09-21 22:09:27 +0100

    added user and subjects

--- cocciep/models/subjects.py
+++ cocciep/models/subjects.py
@@ -1,6 +1,10 @@
 from cocciep.db import db
-
+from datetime import datetime
 class Subject(db.Model):
   __tablename__ = 'subjects'
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(128))
+  created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+  deleted_at = db.Column(db.DateTime(), nullable=True)
+
+# how to create seed automatically  
--- cocciep/models/user.py
+++ cocciep/models/user.py
@@ -1,8 +1,17 @@
 from cocciep.db import db
-
+from datetime import datetime, date
 class User(db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(128))
-  password_hash = db.Column(db.String(218))
-  user_level = db.Column(db.Integer())
+  name = db.Column(db.String(128), nullable=False)
+  gender = db.Column(db.String(1)) #M, F, O
+  gender_other = db.Column(db.String(128))
+  birthdate = db.Column(db.Date(), nullable=False)
+  mobile = db.Column(db.String(15), nullable=False)
+  mobile2 = db.Column(db.String(15))
+  email = db.Column(db.String(128), nullable=False)
+  password_hash = db.Column(db.String(218), nullable=False)
+  active = db.Column(db.Boolean(), nullable=False)
+  user_level = db.Column(db.Integer(), nullable=False, default=1) #1 = teacher | 0=admin
+  created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+  deleted_at = db.Column(db.DateTime(), nullable=True)
