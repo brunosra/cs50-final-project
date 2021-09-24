61d92467d5f5b09b104722a7ffa4239a20589554
Bruno Almeida <bruno@mcontigo.com>
2021-09-24 17:54:00 +0100

    fixed classs to students_class

--- cocciep/models/students_class.py
+++ cocciep/models/students_class.py
@@ -1,8 +1,8 @@
 from cocciep.db import db
 from datetime import datetime
 
-class Class(db.Model):
-  __tablename__ = 'class'
+class StudentsClass(db.Model):
+  __tablename__ = 'students_class'
   id = db.Column(db.Integer(), primary_key=True)
   name = db.Column(db.String(128), unique=True)
   school_year_id = db.Column(db.Integer(), db.ForeignKey('schoolyear.id'), nullable=False) # create relationship
