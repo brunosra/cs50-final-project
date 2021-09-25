8de5e81b19a5747e13f4e8ac0c1e351995085336
albertoturrion <69010355+albertoturrion@users.noreply.github.com>
2021-09-25 14:17:11 +0200

    Daily relationship to enumStatusClass
    Creating the relationship between Daily and EnumStatusClass. It's been created a new property "status" within Daily model

--- cocciep/models/enum_status_class.py
+++ cocciep/models/enum_status_class.py
@@ -7,6 +7,7 @@ class EnumStatusClass(db.Model):
   name = db.Column(db.String(128), nullable=False)
   created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
   deleted_at = db.Column(db.DateTime(), nullable=True)
+  dailies = db.relationship('Daily', backref=status, lazy=True) # Many-to-one relationship (One status can be in many dailies, but one daily only can have one status)
 
 """
 Seed the enum type:
