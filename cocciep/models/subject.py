2108c1c7157e3762f4463c8828d2a186f179666e
albertoturrion <69010355+albertoturrion@users.noreply.github.com>
2021-09-25 13:54:06 +0200

    Adding relationships to Subject model
    As ClassesSubjectsTeachers is a helper table, we need to make the relationship between the 3 models (many-to-many)
    https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/

--- cocciep/models/subject.py
+++ cocciep/models/subject.py
@@ -5,6 +5,8 @@ class Subject(db.Model):
   __tablename__ = 'subject'
   id = db.Column(db.Integer(), primary_key=True)
   name = db.Column(db.String(128))
+  teachers = db.relationship('Teacher', secondary=ClassesSubjectsTeachers, lazy='subquery', backref=db.backref('subject', lazy=True))
+  students = db.relationship('Student', secondary=ClassesSubjectsTeachers, lazy='subquery', backref=db.backref('subject', lazy=True))
   created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
   deleted_at = db.Column(db.DateTime(), nullable=True)
 
