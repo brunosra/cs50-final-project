4d8638142226d8808a4d741ff9057f7deb50204f
albertoturrion <69010355+albertoturrion@users.noreply.github.com>
2021-09-25 13:06:56 +0200

    Update user.py
    Changing user to teacher

--- cocciep/models/user.py
+++ cocciep/models/user.py
@@ -1,7 +1,7 @@
 from cocciep.db import db
 from datetime import datetime
 
-class User(db.Model):
+class Teacher(db.Model):
   __tablename__ = 'user'
   id = db.Column(db.Integer(), primary_key=True)
   name = db.Column(db.String(128), nullable=False)
@@ -13,6 +13,6 @@ class User(db.Model):
   email = db.Column(db.String(128), nullable=False, unique=True)
   password_hash = db.Column(db.String(218), nullable=False)
   active = db.Column(db.Boolean(), nullable=False)
-  user_level = db.Column(db.Integer(), nullable=False, default=1) #1 = teacher | 0=admin
+  teacher_level = db.Column(db.Integer(), nullable=False, default=1) #1 = teacher | 0=admin
   created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
   deleted_at = db.Column(db.DateTime(), nullable=True)
