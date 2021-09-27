d89536b5340c7ad041f0f19c1232ce976749911f
albertoturrion <69010355+albertoturrion@users.noreply.github.com>
2021-09-27 17:58:33 +0200

    New proposal for Enums
    I have read that we need to create as the second way: https://stackoverflow.com/questions/36136112/defining-sqlalchemy-enum-column-with-python-enum-raises-valueerror-not-a-valid

--- cocciep/models/enum_status_class.py
+++ cocciep/models/enum_status_class.py
@@ -7,8 +7,13 @@ class EnumStatusClass(db.Model):
   name = db.Column(db.String(128), nullable=False)
   created_at = db.Column(db.DateTime(), nullable=False, default=datetime.now())
   deleted_at = db.Column(db.DateTime(), nullable=True)
-  dailies = db.relationship('Daily', backref=status, lazy=True) # Many-to-one relationship (One status can be in many dailies, but one daily only can have one status)
-
+ 
+  
+class EnumStatusClass(enum.Enum):
+  ACTIVE = "active",
+  FINISHED = "finished",
+  CANCELLED = "cancelled"
+  
 """
 Seed the enum type:
 active, finished, cancelled 
