3238fac6bc9a7fb55ee05ed3e28cdddb0fd090d6
Bruno Almeida <bruno@mcontigo.com>
2021-10-12 20:41:00 +0100

    adjusted models

--- cocciep/models/classes_subjects_teachers.py
+++ cocciep/models/classes_subjects_teachers.py
@@ -1,8 +1,9 @@
 from cocciep.db import db
+from cocciep.models.enum_subjects import EnumSubjects
 
 class ClassesSubjectsTeachers(db.Model):
     __tablename__ = 'classes_subjects_teachers'
     id = db.Column(db.Integer(), primary_key=True)
     student_class_id = db.Column(db.Integer(), db.ForeignKey('StudentsClass.id'), nullable=False)
-    subject_id = db.Column(db.Integer(), db.ForeignKey('Subject.id'), nullable=False)
+    subject_id = db.Column(db.Enum(EnumSubjects))
     teacher_id = db.Column(db.Integer(), db.ForeignKey('Teacher.id'), nullable=False)
